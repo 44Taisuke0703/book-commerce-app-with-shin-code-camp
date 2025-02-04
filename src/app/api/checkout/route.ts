f3ceddfb5de954b80a15909aad2a162d49738f03
t4415 <t44150703@yahoo.co.jp>
2025-02-04 21:35:37 +0900

    done

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe"
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion: '2025-01-27.acacia' })
 export const POST = async (req: Request) => {
     const baseUrl = 'http://localhost:3000'
     console.log('start');
--- src/app/api/checkout/success/route.ts
+++ src/app/api/checkout/success/route.ts
@@ -3,7 +3,7 @@
 import prisma from "@/app/lib/prisma"
 import { NextResponse } from "next/server"
 import Stripe from "stripe";
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY as string)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion: '2025-01-27.acacia' })
 
 export const POST = async (req: Request) => {
     const { sessionId } = await req.json();
