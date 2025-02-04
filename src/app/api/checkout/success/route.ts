a6bb3b5821b7f3d67d8ffe9fddf94e9162323e9d
t4415 <t44150703@yahoo.co.jp>
2025-02-04 21:17:22 +0900

    done

--- src/app/api/checkout/success/route.ts
+++ src/app/api/checkout/success/route.ts
@@ -3,7 +3,7 @@
 import prisma from "@/app/lib/prisma"
 import { NextResponse } from "next/server"
 import Stripe from "stripe";
-const stripe = new Stripe(process.env.STRIPE_SECRET_KET as string)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export const POST = async (req: Request) => {
     const { sessionId } = await req.json();
